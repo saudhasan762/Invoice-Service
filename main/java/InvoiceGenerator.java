eb11c0dbb972bcca08251f3955d4c43c241a1f6a
saudhasan762 <azanali152@gmail.com>
2021-03-12 21:34:53 +0530

    Calculating Fare for Multiple Rides

--- main/java/InvoiceGenerator.java
+++ main/java/InvoiceGenerator.java
@@ -5,8 +5,14 @@ public class InvoiceGenerator {
 
     public double calculateFare(double distance, int time) {
         double totalFare = distance * MINIMUM_COST_PER_KILOMETER + time * COST_PER_TIME;
-        if(totalFare < MIN_FARE)
-            return MIN_FARE;
+        return Math.max(totalFare, MIN_FARE);
+    }
+
+    public double calculateFare(Ride[] rides) {
+        double totalFare = 0;
+        for (Ride ride:rides) {
+            totalFare += this.calculateFare(ride.distance, ride.time);
+        }
         return totalFare;
     }
 }
--- test/java/InvoiceServiceTest.java
+++ test/java/InvoiceServiceTest.java
@@ -1,24 +1,40 @@
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
 
 public class InvoiceServiceTest {
+    InvoiceGenerator invoiceGenerator = null;
+
+    @Before
+    public void setUp() throws Exception{
+        invoiceGenerator = new InvoiceGenerator();
+    }
 
     @Test
     public void givenDistanceAndTime_ShouldReturnTotalFare(){
-        InvoiceGenerator invoiceGenerator = new InvoiceGenerator();
         double distance = 2.0;
         int time = 5;
         double fare = invoiceGenerator.calculateFare(distance,time);
-        Assertions.assertEquals(25,fare,0.0);
+        Assert.assertEquals(25,fare,0.0);
     }
 
     @Test
     public void givenLessDistanceAndTime_ShouldReturnMinFare(){
-        InvoiceGenerator invoiceGenerator = new InvoiceGenerator();
         double distance = 0.1;
         int time = 1;
         double fare = invoiceGenerator.calculateFare(distance,time);
-        Assertions.assertEquals(5,fare,0.0);
+        Assert.assertEquals(5,fare,0.0);
+    }
 
+    @Test
+    public void givenMultipleRides_ShouldReturnTotalFare(){
+        Ride[] rides = {new Ride(2.0, 5),
+                new Ride(0.1,1)
+        };
+        double fare = invoiceGenerator.calculateFare(rides);
+        Assert.assertEquals(30,fare,0.0);
     }
 }
+
+
